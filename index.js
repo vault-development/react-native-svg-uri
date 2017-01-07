5cf09836e932ab2a8187348bc4817ec2f446d4d7
Henrik Raitasola <henrik.raitasola@gmail.com>
2017-01-07 16:03:54 +0200

    Remove unneeded variable

--- index.js
+++ index.js
@@ -133,8 +133,7 @@ class SvgUri extends Component{
           const attributeValue = node.attributes[i].nodeValue;
           
           if (attribute in ATTS_TRANSFORM) {
-            const transformedAttribute = this.transformSVGAtt(attribute, attributeValue);
-            Object.assign(validAttributes, transformedAttribute);
+            Object.assign(validAttributes, this.transformSVGAtt(attribute, attributeValue));
           } 
           else if (attribute in ATTS_TRANSFORMED_NAMES) {
             validAttributes[ATTS_TRANSFORMED_NAMES[attribute]] = attributeValue;
