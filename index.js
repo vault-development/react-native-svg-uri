c606e19e5b48b471df67b6fb4b8bade6e006e314
Stefan Dobrev <stefan.dobrev@gmail.com>
2017-03-24 12:13:24 +0200

    Fix gradient creation
    Currently gradients were not converted correctly because they are child
    nodes of `<defs>` but defs were not accepted. This change addresses this
    shortcoming.
    
    Additionally respect `gradientUnits` for gradients and `stopColor` for
    gradient stops.

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ const ACEPTED_SVG_ELEMENTS = [
   'circle',
   'path',
   'rect',
+  'defs',
   'linearGradient',
   'radialGradient',
   'stop',
@@ -42,9 +43,9 @@ const G_ATTS = ['id'];
 const CIRCLE_ATTS = ['cx', 'cy', 'r', 'fill', 'stroke'];
 const PATH_ATTS = ['d', 'fill', 'stroke'];
 const RECT_ATTS = ['width', 'height', 'fill', 'stroke', 'x', 'y'];
-const LINEARG_ATTS = ['id', 'x1', 'y1', 'x2', 'y2'];
-const RADIALG_ATTS = ['id', 'cx', 'cy', 'r'];
-const STOP_ATTS = ['offset'];
+const LINEARG_ATTS = ['id', 'x1', 'y1', 'x2', 'y2', 'gradientUnits'];
+const RADIALG_ATTS = ['id', 'cx', 'cy', 'r', 'gradientUnits'];
+const STOP_ATTS = ['offset', 'stopColor'];
 const ELLIPSE_ATTS = ['fill', 'cx', 'cy', 'rx', 'ry'];
 const POLYGON_ATTS = ['points'];
 
@@ -129,12 +130,14 @@ class SvgUri extends Component{
         case 'rect':
              componentAtts = this.obtainComponentAtts(node, RECT_ATTS);
             return <Rect key={i} {...componentAtts}>{childs}</Rect>;
+        case 'defs': 
+            return <Defs key={i}>{childs}</Defs>;
         case 'linearGradient':
              componentAtts = this.obtainComponentAtts(node, LINEARG_ATTS);
-            return <Defs key={i}><LinearGradient {...componentAtts}>{childs}</LinearGradient></Defs>;
+            return <LinearGradient key={i} {...componentAtts}>{childs}</LinearGradient>;
         case 'radialGradient':
              componentAtts = this.obtainComponentAtts(node, RADIALG_ATTS);
-            return <Defs key={i}><RadialGradient {...componentAtts}>{childs}</RadialGradient></Defs>;
+            return <RadialGradient key={i} {...componentAtts}>{childs}</RadialGradient>;
         case 'stop':
              componentAtts = this.obtainComponentAtts(node, STOP_ATTS);
             return <Stop key={i} {...componentAtts}>{childs}</Stop>;
