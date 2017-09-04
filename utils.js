64f0796ccb4b6a05a6007aaa21837ced00918238
Bernd Schrooten <schrootenbernd@gmail.com>
2017-09-04 15:21:27 +0200

    Add support for classes

--- utils.js
+++ utils.js
@@ -19,3 +19,44 @@ export const transformStyle = ({nodeName, nodeValue, fillProp}) => {
 };
 
 export const getEnabledAttributes = enabledAttributes => ({nodeName}) => enabledAttributes.includes(camelCase(nodeName));
+
+export const extractStyleClasses = (node) => {
+
+    if (node.nodeName === 'style') {
+        let stylesString = node.firstChild.nodeValue;
+        let classArray = stylesString.split('}');
+
+        return classArray.reduce((acc, classObj) => {
+            let [className, style] = classObj.split('{');
+            if (className === '') {
+                return acc;
+            }
+            className = className.substring(1);
+            return {...acc, [className]: extractStyle(style)};
+        }, {})
+    } else {
+        if (node.childNodes) {
+            let result = false;
+            for (let i = 0; i < node.childNodes.length; i++) {
+                result = extractStyleClasses(node.childNodes[i]);
+                if (result) {
+                    return result;
+                }
+            }
+        }
+
+        return false;
+    }
+};
+
+export const extractStyle = (style, fillProp) => {
+    return style.split(';')
+        .reduce((acc, attribute) => {
+            const [property, value] = attribute.split(':');
+            if (property === '') {
+                return acc;
+            } else {
+                return {...acc, [camelCase(property)]: fillProp && property === 'fill' ? fillProp : value};
+            }
+        }, {});
+};
